118f1fb5bcfe124d9f3212021a34fb5838190d84
gabriel-poatek <gabriel.ximenes@poatek.com>
2023-12-27 15:11:47 -0300

    di hilt

---
+++ app/src/main/java/br/com/movieapp/movie_popular_feature/data/source/MoviePopularDataSourceImpl.kt
@@ -0,0 +1,21 @@
+package br.com.movieapp.movie_popular_feature.data.source
+
+import br.com.movieapp.core.data.remote.MovieService
+import br.com.movieapp.core.data.remote.response.MoviesResponse
+import br.com.movieapp.core.paging.MoviePagingSource
+import br.com.movieapp.movie_popular_feature.domain.source.MoviePopularRemoteDataSource
+import javax.inject.Inject
+
+
+class MoviePopularDataSourceImpl @Inject constructor(
+    private val service: MovieService
+) : MoviePopularRemoteDataSource {
+
+    override fun getPopularMoviesPagingSource(): MoviePagingSource {
+        return MoviePagingSource(this)
+    }
+
+    override suspend fun getPopularMovies(page: Int): MoviesResponse {
+        return service.getPopularMovies(page)
+    }
+}
