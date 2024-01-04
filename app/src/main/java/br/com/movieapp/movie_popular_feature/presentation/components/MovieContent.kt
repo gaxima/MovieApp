1b31b81d87ecab2c41527d70941511e9673e8bad
gabriel-poatek <gabriel.ximenes@poatek.com>
2024-01-04 16:16:57 -0300

    adding states on list

--- app/src/main/java/br/com/movieapp/movie_popular_feature/presentation/components/MovieContent.kt
+++ app/src/main/java/br/com/movieapp/movie_popular_feature/presentation/components/MovieContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,8 +14,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import br.com.movieapp.core.domain.model.Movie
+import br.com.movieapp.core.presentation.components.common.ErrorScreen
+import br.com.movieapp.core.presentation.components.common.LoadingItem
 
 @Composable
 fun MovieContent(
@@ -46,10 +50,80 @@ fun MovieContent(
                             onClick(movieId)
                         }
                     )
-
                 }
-
             }
+            pagingMovies.apply {
+                when {
+                    loadState.refresh is LoadState.Loading -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            LoadingItem()
+                        }
+                    }
+
+                    loadState.append is LoadState.Loading -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            LoadingItem()
+                        }
+                    }
+
+                    loadState.refresh is LoadState.Error -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            ErrorScreen(message = "verifique sua conexão com a internet",
+                                retry = {
+                                    retry()
+                                })
+                        }
+                    }
+
+                    loadState.append is LoadState.Loading -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            ErrorScreen(
+                                message = "",
+                                retry = {
+                                    retry()
+                                })
+                        }
+                    }
+                }
+            }
+////                    loadState.refresh is androidx.paging.LoadState.Error -> {
+////                        val e = pagingMovies.loadState.refresh as androidx.paging.LoadState.Error
+////                        item {
+////                            ErrorItem(
+////                                message = e.error.localizedMessage ?: "Unknown error",
+////                                modifier = Modifier.fillMaxSize(),
+////                                onClickRetry = { retry() }
+////                            )
+////                        }
+////                    }
+////                    loadState.append is androidx.paging.LoadState.Error -> {
+////                        val e = pagingMovies.loadState.append as androidx.paging.LoadState.Error
+////                        item {
+////                            ErrorItem(
+////                                message = e.error.localizedMessage ?: "Unknown error",
+////                                modifier = Modifier.fillMaxSize(),
+////                                onClickRetry = { retry() }
+////                            )
+////                        }
+////                    }
+////                }
+//            }
         }
     }
 }
