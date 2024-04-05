dde2ae27858246cf3f6727a03e96e5354b5f8d52
gabriel-poatek <gabriel.ximenes@poatek.com>
2024-04-05 16:33:09 -0300

    returning to the initial state

--- commons/build.gradle.kts
+++ commons/build.gradle.kts
@@ -16,11 +16,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        val apiKey = System.getenv("API_KEY") ?: ""
-
-        val baseUrlImage = System.getenv("BASE_URL_IMAGE") ?: ""
-
-        val baseUrl = System.getenv("BASE_URL") ?: ""
 
 //        buildConfigField(
 //            type = "String",
@@ -28,11 +23,16 @@ android {
 //            value = if (project.properties["apiKey"] != "") project.properties["apiKey"].toString() else ""
 //        )
 
-
+        //load the values from .properties file
+        val keystoreFile = project.rootProject.file("apikey.properties")
+        val properties = Properties()
+        properties.load(keystoreFile.inputStream())
+        //return empty key in case something goes wrong
+        val apiKey = properties.getProperty("API_KEY") ?: ""
+        val baseUrlImage = properties.getProperty("BASE_URL_IMAGE") ?: ""
+        val baseUrl = properties.getProperty("BASE_URL") ?: ""
         buildConfigField(type = "String", name = "API_KEY", value = apiKey)
-
         buildConfigField(type = "String", name = "BASE_URL_IMAGE", value = baseUrlImage)
-
         buildConfigField(type = "String", name = "BASE_URL", value = baseUrl)
 
     }
--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -16,16 +16,16 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        val apiKey = System.getenv("API_KEY") ?: ""
-
-        val baseUrlImage = System.getenv("BASE_URL_IMAGE") ?: ""
-
-        val baseUrl = System.getenv("BASE_URL") ?: ""
-
+        //load the values from .properties file
+        val keystoreFile = project.rootProject.file("apikey.properties")
+        val properties = Properties()
+        properties.load(keystoreFile.inputStream())
+        //return empty key in case something goes wrong
+        val apiKey = properties.getProperty("API_KEY") ?: ""
+        val baseUrlImage = properties.getProperty("BASE_URL_IMAGE") ?: ""
+        val baseUrl = properties.getProperty("BASE_URL") ?: ""
         buildConfigField(type = "String", name = "API_KEY", value = apiKey)
-
         buildConfigField(type = "String", name = "BASE_URL_IMAGE", value = baseUrlImage)
-
         buildConfigField(type = "String", name = "BASE_URL", value = baseUrl)
     }
 
