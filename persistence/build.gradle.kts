f5d8b4ef9252c928d76152c30912425763db2b4f
gabriel-ximenes-telus <gabriel.ximenes@telus.com>
2024-05-21 14:01:53 -0300

    Changing the instrumented tests of Room to Unit tests using Roboletric

--- persistence/build.gradle.kts
+++ persistence/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
     id("org.sonarqube")
 }
 
@@ -12,14 +13,16 @@ android {
     defaultConfig {
         minSdk = AppConfig.minSdkVersion
 
-        //testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        testInstrumentationRunner = "br.com.movieapp.HiltTestRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//        testInstrumentationRunner = "br.com.movieapp.HiltTestRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-
+        }
+        debug {
+            enableAndroidTestCoverage = true
         }
     }
     compileOptions {
@@ -29,6 +32,11 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
 }
 
 dependencies {
@@ -37,10 +45,10 @@ dependencies {
     implementation(Libs.Kotlin.getAppCompact())
     implementation(Libs.Material.getMaterialVersion())
 
-
     //Room
     implementation(Libs.Room.getRoomVersion())
     implementation(Libs.Room.getRoomRuntimeVersion())
+    implementation("androidx.test:runner:1.5.2")
     ksp(Libs.Room.getRoomCompilerVersion())
 
     //Hilt
@@ -49,25 +57,33 @@ dependencies {
     ksp(Libs.DaggerHilt.getDaggerHiltCompilerVersion())
     ksp(Libs.Hilt.getHiltCompilerVersion())
 
-
     testImplementation(Libs.Test.getJunitVersion())
     implementation(Libs.Truth.getTruthVersion())
-    androidTestImplementation(Libs.DaggerHilt.getDaggerHiltTesting())
-    kspAndroidTest(Libs.DaggerHilt.getDaggerHiltCompilerVersion())
-    androidTestImplementation(Libs.Kotlin.getCoreTesting())
-    androidTestImplementation(Libs.Coroutines.getTestVersion())
-    androidTestImplementation(Libs.Truth.getTruthVersion())
+//    androidTestImplementation(Libs.DaggerHilt.getDaggerHiltTesting())
+    testImplementation(Libs.DaggerHilt.getDaggerHiltTesting())
+//    kspAndroidTest(Libs.DaggerHilt.getDaggerHiltCompilerVersion())
+    kspTest(Libs.DaggerHilt.getDaggerHiltCompilerVersion())
+//    androidTestImplementation(Libs.Kotlin.getCoreTesting())
+    testImplementation(Libs.Kotlin.getCoreTesting())
+//    androidTestImplementation(Libs.Coroutines.getTestVersion())
+    testImplementation(Libs.Coroutines.getTestVersion())
+//    androidTestImplementation(Libs.Truth.getTruthVersion())
+    testImplementation(Libs.Truth.getTruthVersion())
+
 
     testImplementation(Libs.Mockito.getMockitoVersion())
-    androidTestImplementation(Libs.Mockito.getMockitoVersion())
+//    androidTestImplementation(Libs.Mockito.getMockitoVersion())
+    testImplementation(Libs.Mockito.getMockitoVersion())
     testImplementation(Libs.Mockito.getMockitoInlineVersion())
 
+//    androidTestImplementation(Libs.Test.getExtJunitVersion())
+    testImplementation(Libs.Test.getExtJunitVersion())
+//    androidTestImplementation(Libs.Test.getEspressoVersion())
+     testImplementation(Libs.Test.getEspressoVersion())
 
-    androidTestImplementation(Libs.Test.getExtJunitVersion())
-    androidTestImplementation(Libs.Test.getEspressoVersion())
-
-    androidTestImplementation(Libs.Test.getComposeUiTestVersion())
-
+//    androidTestImplementation(Libs.Test.getComposeUiTestVersion())
+    testImplementation(Libs.Test.getComposeUiTestVersion())
+    testImplementation(Libs.Test.getRoboletricVersion())
 
     debugImplementation(Libs.Compose.getComposeToolingVersion())
     debugImplementation(Libs.Compose.getComposeUiTestManifestVersion())
